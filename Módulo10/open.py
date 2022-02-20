a9c5c9a0edeae63cd7225e4fcc9296fe29cfa5ea
Sinthya Bermudez <sbermudezr1100@alumno.ipn.mx>
2022-02-20 08:18:13 -0600

    add: files from cloud gui

---
+++ Módulo10/open.py
@@ -0,0 +1,82 @@
+def main():
+    open("/path/to/mars.jpg")
+
+if __name__ == '__main__':
+    main()
+
+##parte1
+
+$ python3 open.py
+Traceback (most recent call last):
+  File "/tmp/open.py", line 5, in <module>
+    main()
+  File "/tmp/open.py", line 2, in main
+    open("/path/to/mars.jpg")
+FileNotFoundError: [Errno 2] No such file or directory: '/path/to/mars.jpg'
+
+##parte 2
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+    >>> water_left(5, 100, 2)
+'Total water left after 2 days is: -10 liters'
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+>>> water_left(5, 100, 2)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<stdin>", line 6, in water_left
+RuntimeError: There is not enough water for 5 astronauts after 2 days!
+
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
+
+>>> water_left("3", "200", None)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<stdin>", line 3, in water_left
+TypeError: can't multiply sequence by non-int of type 'NoneType'
+
+
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            # If argument is an int, the following operation will work
+            argument / 10
+        except TypeError:
+            # TypError will be raised only if it isn't the right type 
+            # Raise the same exception but with a better error message
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+
+    >>> water_left("3", "200", None)
+Traceback (most recent call last):
+  File "<stdin>", line 5, in water_left
+TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+During handling of the preceding exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<stdin>", line 9, in water_left
+TypeError: All arguments must be of type int, but received: '3'
